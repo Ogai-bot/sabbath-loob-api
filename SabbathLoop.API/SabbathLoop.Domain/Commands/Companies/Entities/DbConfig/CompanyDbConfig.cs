7d28730e9ab8c87c268382045e50f54fdf2e062a
thiago <thiago.cunha20@outlook.com>
2022-11-16 06:52:58 -0300

    First database update

--- SabbathLoop.API/SabbathLoop.Domain/Commands/Companies/Entities/DbConfig/CompanyDbConfig.cs
+++ SabbathLoop.API/SabbathLoop.Domain/Commands/Companies/Entities/DbConfig/CompanyDbConfig.cs
@@ -10,10 +10,11 @@ namespace SabbathLoop.Domain.Commands.Companies.Entities.DbConfig
         {
             builder.ToTable("companies", "dbo");
 			builder.HasKey(b => b.Id).HasName("PK_companies");
+            builder.Property(b => b.Id).HasColumnName("id").HasColumnType("uniqueidentifier").HasDefaultValueSql("NEWID()").IsRequired();
             builder.Property(b => b.Name).HasColumnName("name").HasColumnType("varchar(100)").IsRequired();
             builder.Property(b => b.CreationDate).HasColumnName("creation_date").HasColumnType("datetimeoffset").HasDefaultValueSql("SYSDATETIME()").IsRequired();
             builder.Property(b => b.LastModified).HasColumnName("last_modified").HasColumnType("datetimeoffset");
-            builder.Property(b => b.Removed).HasColumnName("removed").HasColumnType("bit").HasDefaultValueSql("0").IsRequired();
+            builder.Property(b => b.Removed).HasColumnName("removed").HasColumnType("bit").IsRequired();
             builder.HasQueryFilter(b => !b.Removed);
         }
     }
---
+++ SabbathLoop.API/SabbathLoop.Domain/Migrations/20221116004659_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SabbathLoop.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "companies",
+                schema: "dbo",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    lastmodified = table.Column<DateTimeOffset>(name: "last_modified", type: "datetimeoffset", nullable: true),
+                    creationdate = table.Column<DateTimeOffset>(name: "creation_date", type: "datetimeoffset", nullable: false, defaultValueSql: "SYSDATETIME()"),
+                    removed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_companies", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "companies",
+                schema: "dbo");
+        }
+    }
+}
